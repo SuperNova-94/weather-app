c3053b480e8cd16fd977588a35e2a5a2da07a89c
SuperNova-94 <johnwinchester21600@gmail.com>
2021-11-07 07:15:03 -0800

    03 - Displayed weather data in html document

--- script.js
+++ script.js
@@ -2,7 +2,7 @@
  * Weather App
  * DONE: Complete getWeatherData() to return json response Promise ✔
  * DONE: Complete searchCity() to get user input and get data using getWeatherData() ✔
- * TODO: Complete showWeatherData() to set the data in the the html file from response
+ * DONE: Complete showWeatherData() to set the data in the the html file from response ✔
  */
 
 // API_KEY for maps api
@@ -17,7 +17,7 @@ let API_KEY = "a8e71c9932b20c4ceb0aed183e6a83bb";
 getWeatherData = (city) => {
   const URL = "https://api.openweathermap.org/data/2.5/weather";
   //HINT: Use template literals to create a url with input and an API key
-  const FULL_URL = `${URL}?q=${city}&appid=${API_KEY}&units=celcius`
+  const FULL_URL = `${URL}?q=${city}&appid=${API_KEY}&units=metric`
   const weatherPromise = fetch(FULL_URL);
   return weatherPromise.then((response) => {
     return response.json();
@@ -33,6 +33,7 @@ searchCity = () => {
   getWeatherData(city)
   .then((response) => {
     console.log(response);
+    showWeatherData(response);
   }).catch((error) => {
     console.log(error);
   })
@@ -44,7 +45,10 @@ searchCity = () => {
  * HINT: make sure to console log the weatherData to see how the data looks like
  */
 showWeatherData = (weatherData) => {
-  //CODE GOES HERE
+  document.getElementById('city-name').innerText = weatherData.name;
+  document.getElementById('weather-type').innerText = weatherData.weather[0].main;
+  document.getElementById('temp').innerText = weatherData.main.temp;
+  document.getElementById('min-temp').innerText = weatherData.main.temp_min;
+  document.getElementById('max-temp').innerText = weatherData.main.temp_max;
   
 }
-
